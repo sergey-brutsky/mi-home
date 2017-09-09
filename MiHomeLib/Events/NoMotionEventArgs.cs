f7ad220d089387817e7f1ffcb5d039f9f0d61c66
Sergey Brutsky <s.brutsky@adform.com>
2017-09-09 22:29:23 +0300

    Added support for motion events

---
+++ MiHomeLib/Events/NoMotionEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MiHomeLib.Events
+{
+    public class NoMotionEventArgs: EventArgs
+    {
+        public NoMotionEventArgs(int seconds)
+        {
+            Seconds = seconds;
+        }
+
+        public int Seconds { get; }
+    }
+}
