4441e1412d57fa60a5fdf775bd8247ac19154ee6
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-03-25 18:54:19 +0800

    + Added WiredDualWallSwitch

---
+++ MiHomeLib/Devices/WiredDualWallSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace MiHomeLib.Devices
+{
+    public class WiredDualWallSwitch : MiHomeDevice
+    {
+        public event EventHandler OnSwitchChannelRight;
+        public event EventHandler OnSwitchChannelLeft;
+
+        public WiredDualWallSwitch(string sid) : base(sid, "ctrl_neutral2") 
+        {
+            StatusLeft = "idle";
+            StatusRight = "idle";
+        }
+
+        public float? Voltage { get; set; }
+
+        public string StatusLeft { get; private set; }
+        public string StatusRight { get; private set; }
+
+        public override void ParseData(string command)
+        {
+            Console.WriteLine(command);
+            var jObject = JObject.Parse(command);
+
+            if (jObject["status"] != null)
+            {
+
+            }
+
+            if (jObject["voltage"] != null && float.TryParse(jObject["voltage"].ToString(), out float v))
+            {
+                Voltage = v / 1000;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Status Left: {StatusLeft}, Right: {StatusRight}, Voltage: {Voltage}V";
+        }
+    }
+}
