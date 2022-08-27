94824d02433e10080ea972751900ce80bb435dc4
Siarhei Brutski <brutsky.sergey@gmail.com>
2022-08-28 01:10:59 +0200

    Refactored code to add support for xiaomi gateway 3

--- MiHomeConsole/Program.cs
+++ MiHomeConsole/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
 using MiHomeLib;
 
 namespace MiHomeConsole
@@ -7,13 +11,11 @@ namespace MiHomeConsole
     {
         public static void Main(string[] args)
         {
-            //Action<ILoggingBuilder> loggingBuilder =
-            //    builder => builder.AddConsole(x =>
-            //    {
-            //        x.DisableColors = true;
-            //        x.Format = ConsoleLoggerFormat.Systemd;
-            //        x.TimestampFormat = " yyyy-MM-d [HH:mm:ss] - ";
-            //    });
+            Action<ILoggingBuilder> loggingBuilder =
+                builder => builder.AddSystemdConsole(x =>
+                {
+                    x.TimestampFormat = " yyyy-MM-d [HH:mm:ss] - ";
+                });
 
             //MiHome.LoggerFactory = LoggerFactory.Create(loggingBuilder);
             //MiHome.LogRawCommands = true;
@@ -21,12 +23,16 @@ namespace MiHomeConsole
             // pwd of your gateway (optional, needed only to send commands to your devices) 
             // and sid of your gateway (optional, use only when you have 2 gateways in your LAN)
             //using var miHome = new MiHome("pwd", "sid")
-            using var miHome = new MiHome();
+            //using var miHome = new MiHome();
 
-            miHome.OnAnyDevice += (_, device) =>
-            {
-                Console.WriteLine($"{device.Sid}, {device.GetType()}, {device}"); // all discovered devices
-            };
+            //miHome.OnAnyDevice += (_, device) =>
+            //{
+            //    Console.WriteLine($"{device.Sid}, {device.GetType()}, {device}"); // all discovered devices
+            //};
+
+            MiHome3.LoggerFactory = LoggerFactory.Create(loggingBuilder);
+
+            using var miHome = new MiHome3("192.168.5.109");
 
             Console.ReadLine();
         }
---
+++ MiHomeLib/MiHome3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MiHomeLib.Commands;
+using MiHomeLib.Contracts;
+using MiHomeLib.Devices;
+using MiHomeLib.Events;
+using Newtonsoft.Json.Linq;
+using MQTTnet.Client;
+using MQTTnet.Packets;
+using MQTTnet;
+using MQTTnet.Server;
+using System.Text;
+
+namespace MiHomeLib
+{
+    public class MiHome3 : IDisposable
+    {
+        private IMqttClient _mqttClient;
+        private static ILogger _logger;
+        private static ILoggerFactory _loggerFactory;        
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public static ILoggerFactory LoggerFactory
+        {
+            set
+            {
+                _loggerFactory = value;
+                _logger = _loggerFactory.CreateLogger<MiHome3>();
+            }
+            get
+            {
+                return _loggerFactory;
+            }
+        }
+
+        public MiHome3(string ip, int port = 1883)
+        {
+            MqttFactory mqttFactory = new MqttFactory();
+
+            _mqttClient = mqttFactory.CreateMqttClient();
+
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(ip, port)
+                .Build();
+
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(x => { x.WithTopic("#"); })
+                .Build();
+
+            _mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                var str = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                //Console.WriteLine(str);
+                _logger.LogInformation($"{e.ApplicationMessage.Topic} --> {str}");
+                return Task.CompletedTask;
+            };
+
+            Task.Run(async () =>
+            {
+                if(!_cts.IsCancellationRequested)
+                {
+                    await _mqttClient.ConnectAsync(mqttClientOptions, _cts.Token);
+                    _logger.LogInformation("MQTT client connected");
+                    await _mqttClient.SubscribeAsync(mqttSubscribeOptions, _cts.Token);
+                    _logger.LogInformation("MQTT client subscribed to the root topic");
+                }
+            }, _cts.Token);
+        }
+
+        public void Dispose()
+        {
+            _cts?.Cancel();
+            _mqttClient?.Dispose();
+        }
+    }
+}
